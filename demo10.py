44ae68feba1854f1e33ae699cfa966ccac1a428c
Germey <cqc@cuiqingcai.com>
2020-04-17 01:06:18 +0800

    init

---
+++ demo10.py
@@ -0,0 +1,50 @@
+html = '''<div id="songs-list">
+<h2 class="title">经典老歌</h2>
+<p class="introduction">
+经典老歌列表
+</p>
+<ul id="list" class="list-group">
+<li data-view="2">一路上有你</li>
+<li data-view="7">
+<a href="/2.mp3" singer="任贤齐">沧海一声笑</a>
+</li>
+<li data-view="4" class="active">
+<a href="/3.mp3" singer="齐秦">往事随风</a>
+</li>
+<li data-view="6"><a href="/4.mp3" singer="beyond">光辉岁月</a></li>
+<li data-view="5"><a href="/5.mp3" singer="陈慧琳">记事本</a></li>
+<li data-view="5">
+<a href="/6.mp3" singer="邓丽君">但愿人长久</a>
+</li>
+</ul>
+</div>'''
+import re
+
+result = re.search('<li.*?active.*?singer="(.*?)">(.*?)</a>', html, re.S)
+if result:
+    print(result.group(1), result.group(2))
+
+result = re.search('<li.*?singer="(.*?)">(.*?)</a>', html, re.S)
+if result:
+    print(result.group(1), result.group(2))
+    
+result = re.search('<li.*?singer="(.*?)">(.*?)</a>', html)
+if result:
+    print(result.group(1), result.group(2))
+    
+results = re.findall('<li.*?href="(.*?)".*?singer="(.*?)">(.*?)</a>', html, re.S)
+print(results)
+print(type(results))
+for result in results:
+    print(result)
+    print(result[0], result[1], result[2])
+    
+results = re.findall('<li.*?>\s*?(<a.*?>)?(\w+)(</a>)?\s*?</li>', html, re.S)
+for result in results:
+    print(result[1])
+    
+html = re.sub('<a.*?>|</a>', '', html)
+print(html)
+results = re.findall('<li.*?>(.*?)</li>', html, re.S)
+for result in results:
+    print(result.strip())
